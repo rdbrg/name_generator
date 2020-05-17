9df7eb76c1e43ebf5786f7fa42950118dfa7b1b8
Vadim Rodberg <vadimrodberg@gmail.com>
2020-05-17 10:18:49 +0300

    Add main code

---
+++ git.py
@@ -0,0 +1,50 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+import random
+import names_list
+
+
+class Main(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        tk.Tk.title(self, "Name Generator")
+        w = 300
+        h = 120
+        tk.Tk.geometry(self, f"{w}x{h}")
+        tk.Tk.resizable(self, False, False)
+
+        self.init_widgets()
+
+    def init_widgets(self):
+        self.main_frame = ttk.Frame(self)
+        self.main_frame.pack(padx=X, pady=Y, expand=1, fill=tk.BOTH)
+
+        self.lab_output = ttk.Label(self.main_frame, font=20)
+        self.lab_output.grid(row=0, column=0, sticky=tk.W, columnspan=3)
+
+        self.var_ch = tk.StringVar()
+        self.var_ch.set("male")
+        cb_sex_choice_male = ttk.Radiobutton(self.main_frame, text="Male", variable=self.var_ch, value="male")
+        cb_sex_choice_male.grid(row=1, column=0, sticky=tk.W)
+
+        cb_sex_choice_female = ttk.Radiobutton(self.main_frame, text="Female", variable=self.var_ch, value="female")
+        cb_sex_choice_female.grid(row=2, column=0, sticky=tk.W)
+
+        btn_generate = ttk.Button(self.main_frame, text="Generate", command=self.generate_name)
+        btn_generate.grid(row=3, column=0, sticky=tk.W)
+
+    def generate_name(self):
+        if self.var_ch.get() == "male":
+            self.lab_output.configure(text=f"{random.choice(names_list.name_male_list)} "
+                                           f"{random.choice(names_list.surname_male_list)}")
+        else:
+            self.lab_output.configure(text=f"{random.choice(names_list.name_female_list)} "
+                                           f"{random.choice(names_list.surname_female_list)}")
+
+
+if __name__ == '__main__':
+    X = 20
+    Y = 10
+    app = Main()
+    app.mainloop()
